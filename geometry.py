6b30caa89c55daaad2e1668014b5c9e58523c088
Alice <alicescarpa@gmail.com>
2014-12-23 17:05:54 -0500

    Added a sphere class

--- geometry.py
+++ geometry.py
@@ -7,7 +7,7 @@ class Line():
         self.slope = slope
         self.start = start
 
-    @static
+    @staticmethod
     def throughPoints(point0, point1):
         """
         Creates a line that passes in two given points 
@@ -16,3 +16,31 @@ class Line():
         x0, y0, z0 = point0
         return Line((x1 - x0, y1 - y0, z1 - z0), (x0, y0, z0))
 
+    def distanceToPoint(self, point):
+        """
+        Returns the distance between a line and a point
+        currently broken
+        """
+        return 1
+
+class Sphere():
+    def __init__(self, center, radius):
+        """
+        Center is an 3-element tuple
+        Radius is a positive number
+        """
+        self.center = center
+        self.radius = radius
+
+    @staticmethod
+    def new_sphere(center, radius):
+        return Sphere(center, radius)
+
+def intersect(line, sphere):
+    return line.distanceToPoint(sphere.center) <= sphere.radius
+
+S = Sphere.new_sphere((0, 0, 0), 1)
+S2 = Sphere.new_sphere((0, 0, 0), 0.5)
+l = Line.throughPoints((-10, 0, 0), (1, 1, 1))
+print intersect(l, S)
+print intersect(l, S2)
